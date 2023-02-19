bf36e903a2e7c4248cb18a1b69c9b3bd3b28b8dc
MariamO6274 <ostatishvili@gmail.com>
2023-02-18 19:15:00 -0700

    header part and click event

---
+++ script.js
@@ -0,0 +1,24 @@
+const moon = document.querySelector("#moon");
+const sun = document.querySelector("#sun");
+const title = document.querySelector(".title");
+const themeText = document.querySelector(".theme-text");
+const input = document.querySelector("#user");
+const button = document.querySelector(".btn");
+
+const flipTheme = (theme) => {
+  if (theme === "dark") {
+    moon.style.display = "none";
+    sun.style.display = "block";
+    document.body.style.backgroundColor = "#141D2F";
+  } else {
+  }
+  title.classList.toggle("dark");
+  themeText.classList.toggle("dark");
+};
+
+moon.addEventListener("click", () => flipTheme("dark"));
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(input.value);
+});
