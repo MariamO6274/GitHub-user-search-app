13cd4fb1f583f07d2fd15057363d9bd30561dbc7
MariamO6274 <ostatishvili@gmail.com>
2023-02-19 18:23:48 -0700

    Dark/Light mode

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const blog = document.querySelector("#blog");
 const twitter = document.querySelector("#twitter");
 const company = document.querySelector("#company");
 const errorElement = document.querySelector(".error");
+const stats = document.querySelector(".statistic");
 
 const octocat = {
   avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
@@ -123,6 +124,9 @@ const flipTheme = (theme) => {
   themeText.classList.toggle("dark");
   input.classList.toggle("dark");
   Array.from(cards).forEach((card) => card.classList.toggle("dark"));
+  blog.classList.toggle("dark");
+  login.style.color = "var(--electric)";
+  stats.classList.toggle("dark");
 };
 
 moon.addEventListener("click", () => flipTheme("dark"));
